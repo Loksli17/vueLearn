08d5dcf8e556174076a58be822acb68169b8e7f3
RobinLoksli <ami0504@mail.ru>
2021-09-30 23:56:02 +1000

    edit user full

---
+++ client/src/views/seq/Edit.vue
@@ -0,0 +1,84 @@
+
+<template>
+    
+    <div class="seq-edit">
+
+        <h1>Edit item page</h1>
+
+        <div class="add-line">
+            <router-link :to="'/'">Go home</router-link>
+            <router-link :to="'/seq-crud'">Go index-crud</router-link>
+        </div>
+
+
+        <div class="form-wrap">
+
+            <div class="form-wrap">
+                <Form 
+                    v-if="scheme && dataForm"
+                    :scheme="scheme"
+                    :data="dataForm"
+                    :tableName="'user'"
+                    v-on:send="sendForm"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+    import { AxiosResponse }                    from 'axios';
+    import { defineComponent }                  from 'vue';
+    import Form, { FormHtmlItem, FormDataView } from '../../components/crudComponent/newForm.vue';
+    import UserService                          from '../../services/UserService';
+    import FlashMessageData                     from '../../libs/flashMessage';
+
+
+    export default defineComponent({
+
+        components: {
+            Form,
+        },
+        
+        data: function(){
+            return {
+                user    : {} as Record<string, any>,
+                scheme  : [] as Array<Array<FormHtmlItem>> | null,
+                dataForm: null as FormDataView | null, 
+            }
+        },
+
+        mounted: async function(){
+            await this.getUser();
+            this.initScheme();
+            this.initFormData();
+
+            console.log(this.scheme, this.dataForm);
+        },
+
+        methods: {
+
+            getUser: async function(){
+                this.user = await UserService.getOne({id: this.$route.params.id});
+            },
+
+            initScheme: function(){
+                this.scheme = [
+                    [{type: 'text', name: 'login', label: 'Login'}, {type: 'text', name: 'email', label: 'email'}],
+                    [{type: 'submit', name: 'sendUser'}]
+                ]
+            },
+
+            initFormData: function(){
+                this.dataForm = this.user as FormDataView;
+            },
+
+            sendForm: async function(data: FormDataView){
+                const res: AxiosResponse = await UserService.editUser({user: data}, this.$route.params.id);
+                this.$flashMessage.show(FlashMessageData.successMessage('Edit user', `User with id = ${this.$route.params.id} was edit`));
+                
+            }
+        },
+    });
+</script>
