447f298469e7ca828bc8544ebdbd481d8fe5c4ca
BlackLirium <mrfilin97@gmail.com>
2021-10-02 15:47:44 +1000

    service book

---
+++ client/src/services/BooksService.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import Filters                  from '@/libs/filters';
+import { LoadingFile }          from '@/components/FileUpload/types';
+import Service                  from '@/libs/Service';
+
+export default class BooksService extends Service{
+    
+    public static async getAll(data: Record<string, any>): Promise<Array<Record<string, any>> | null>{
+
+        const response = await axios.post('/books', data)
+        .catch((reason: AxiosError) => {
+            if(reason.response == undefined) return;
+            this.errorMessage(reason.response.status);
+            console.error(reason);
+        });
+
+        if(response == undefined) { console.error('Bad response'); return null; }
+
+        this.checkResponse(response, [200]);
+
+        return response.data.users;
+    }
+}
