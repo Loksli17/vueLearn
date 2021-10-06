61f7da7ce62bae5b7e49d96f8ca9b338f5c247fd
RobinLoksli <ami0504@mail.ru>
2021-10-06 11:32:59 +1000

    + token

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,39 @@
 <template>
     <div class="app-wrap">
         <FlashMessage class="flash-message" position="right bottom"/>
-        <router-link :to="'/logout'"> Logout => </router-link>
+        <div class="app-line">
+            <router-link :to="'/logout'"> Logout => </router-link>
+            <span>{{token}}</span>
+        </div>
         <router-view/>
     </div>
 </template>
+
+
+<script lang="ts">
+
+    import store from './store';
+
+    export default {
+        data: function(){
+            return {
+                token: store.getters.getJWT,
+            }
+        }
+    }
+</script>
 
 
 <style lang="scss">
     .app-wrap{
         padding: 20px 50px;
         box-sizing: border-box;
+    }
+
+    .app-line{
+        display: grid;
+        grid-auto-flow: column;
+        grid-auto-columns: max-content;
+        column-gap: 20px;
     }
 </style>
